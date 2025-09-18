3d952b9cdc0ced55e693429482f012b93248e71d
Mohamed Amine Boulanouar <moboulan@e1r7p4.1337.ma>
2025-09-18 11:29:58 +0100

    starting point

--- src/repl.test.ts
+++ src/repl.test.ts
@@ -6,13 +6,19 @@ describe.each([
     input: "  hello  world  ",
     expected: ["hello", "world"],
   },
-  // TODO: more test cases here
+  {
+    input: "  hello  world  ",
+    expected: ["hello", "world"],
+  },
+  {
+    input: "  HellO  World  ",
+    expected: ["hello", "world"],
+  },
 ])("cleanInput($input)", ({ input, expected }) => {
   test(`Expected: ${expected}`, () => {
     let actual = cleanInput(input);
     expect(actual).toHaveLength(expected.length);
     for (const i in expected) {
-      // likewise, the `toBe` function will fail the test if the values are not equal
       expect(actual[i]).toBe(expected[i]);
     }
   });
--- src/repl.ts
+++ src/repl.ts
@@ -1,6 +1,28 @@
+import { createInterface } from "node:readline";
+
+export function startREPL(): void {
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "Pokedex > ",
+    });
+
+    rl.prompt();
+
+    rl.on("line", (line) => {
+        const words = cleanInput(line);
+        if (words.length !== 0)
+            console.log(`Your command was: ${words[0]}`);
+        
+        rl.prompt();
+    });
+}
+
 export function cleanInput(input: string): string[] {
     return input
            .trim()
+           .toLowerCase()
            .split(" ")
-           .filter((word) => word !== '');
+           .filter((word) => word !== "");
 }
+
